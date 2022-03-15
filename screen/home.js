193ee21bdb501d9ff700aa2744904129bdd60974
Aslı Cönk <75905402+asliconk@users.noreply.github.com>
2022-03-15 04:31:11 +0300

    initial commit

--- screen/home.js
+++
@@ -1,9 +0,0 @@
-import  React from 'react';
-import { TextInput, View, StyleSheet, TouchableOpacity,Text } from 'react-native';
-export default function home(){
-    return(
-<View>
-    <Text> HOME PAGE </Text>
-</View>
-    );
-}
--- screen/signup.js
+++
@@ -1,6 +0,0 @@
-
-import  React from 'react';
-import { TextInput, View, StyleSheet, TouchableOpacity,Text } from 'react-native';
-export default function signup (){
-    return 
-}
